17a6ba58eefd8b2e291080b984aa5d95038c69f6
Harry <hc9g25@soton.ac.uk>
2025-11-24 09:34:10 +0000

    added arguement check to functions add

--- add.py
+++ add.py
@@ -1,5 +1,5 @@
 def add(x: int, y: int) -> int:
-    if not isinstance(x,int) or not is instance(y,int):
+    if not isinstance(x,int) or not isinstance(y,int):
         raise TypeError("Both arguements must be integers")
     """Returns the sum of two integers."""
     return x + y
